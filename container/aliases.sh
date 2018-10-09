4224cff2f4df3a2fb126201f2b7a7d1489e141cb
Siarhei Piashko <Siarhei.Piashko@ihg.com>
2018-10-09 15:38:38 +0300

    small refactoring

--- container/aliases.sh
+++
@@ -1,2 +0,0 @@
-alias tslog='tail -f /$SERVER_NAME/$SERVER_NAME.log'
-alias tllog='tail -f /$LOCATOR_NAME/$LOCATOR_NAME.log' 
---
+++ container/bashrc.sh
@@ -0,0 +1,2 @@
+alias stlog='tail -f /$SERVER_NAME/$SERVER_NAME.log'
+alias ltlog='tail -f /$LOCATOR_NAME/$LOCATOR_NAME.log' 
