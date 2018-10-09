2fe596daa378b8eb0c8eea4050f2f5b550e8bf74
Siarhei Piashko <Siarhei.Piashko@ihg.com>
2018-10-09 15:14:39 +0300

    initial commit

---
+++ container/aliases.sh
@@ -0,0 +1,2 @@
+alias tslog='tail -f /$SERVER_NAME/$SERVER_NAME.log'
+alias tllog='tail -f /$LOCATOR_NAME/$LOCATOR_NAME.log' 
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -it --rm --name geode-test-server geode-test-server 
